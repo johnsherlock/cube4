2c54d842246a90e25d3924d9d45aad67dbfe891c
John Sherlock <john.sherlock@gmail.com>
2026-01-11 23:49:42 +0000

    Add Capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.johnsherlock.cube4",
+  appName: "CUBE4",
+  webDir: "dist",
+  bundledWebRuntime: false
+};
+
+export default config;
